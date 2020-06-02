60e4a6c2470e6c34b3ed467da7658626833f266b
Steve Roehl <stevenroehl@gmail.com>
2020-06-02 12:24:59 -0500

    Removing unneeded settings from config.py

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ SECRET_KEY = 'you-will-never-guess'
 PROD = True
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-if PROD:
-  SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@db:3306/baby_journal'
-else:
-  SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@db-dev:3306/baby_journal'
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.getenv("BABY_DB_LOC"))
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False
