0829e554bd22d017c8e160890b807fef23f0c519
Steve Roehl <stevenroehl@gmail.com>
2020-07-22 13:43:19 -0500

    Changes to make it work for Docker and upgrade python from 3.4 to 3.6

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-from python:3.4
+from python:3.6
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app/
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+#source /home/steve/venv/baby_journal/bin/activate
+export BABY_DB_LOC=/data/baby_journal.db
+
 gunicorn --bind 0.0.0.0:5000 --certfile cert/certificate.pem --keyfile cert/private-key.pem run:app
